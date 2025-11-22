b085bf3b8a84e070771c29974f20a81fe7d93817
Leyser <R2leyser@proton.me>
2025-11-22 13:55:47 -0300

    Começar IPC e layout das Salas

--- src/agente/main.py
+++ src/agente/main.py
@@ -1,4 +1,44 @@
+#!/usr/bin/env python3.13
+import sys
+import json
+
+# JSON para criar a classe Sala
+# {
+#     "acao": "criar_sala",
+#     "sala": {
+#         "nome": String,
+#         "alunos": [
+#                 { "nome": String,
+#                  "cadastro": Integer
+#                 }
+#             ]
+#         "materia": String
+#     }
+# }
+
+# Json para executar um comando
+# {
+#     "acao": "adicionar_aluno",
+#     "sala": String,
+#     "aluno": {
+#         "nome": String,
+#         "cadastro": Integer
+#     }
+# }
+
+# Json para adicionar PDF
+# {
+#     "sala": String,
+#     "pdf": {
+#         "nome": String,
+#         "url": String
+#     }
+# }
 
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    # Recieve json from stdin
+    input_data = sys.stdin.read()
+    data = json.loads(input_data)
+
+    print(json.dumps(data, indent=4))
